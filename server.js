e28f6351a9201e0aa81399ebe5cd4809c6b3553b
Rabbit <Corbineau@users.noreply.github.com>
2019-06-09 18:49:48 -0400

    server party

---
+++ server.js
@@ -0,0 +1,51 @@
+
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var config = require("./config");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3000;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// Import routes and give the server access to them.
+var routes = require("./controller/controller.js");
+
+app.use(routes);
+
+// Static directory
+app.use(express.static("./public"));
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  
+  console.log("App listening on PORT: " + PORT);
+});
